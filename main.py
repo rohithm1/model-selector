73e0965501fca487a5595ff22f7c6a4e8efa78d9
Rohith Mandavilli <Rohith.Mandavilli.22@Dartmouth.edu>
2024-01-25 15:45:14 -0600

    init

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class ModelSelector:
+
+    def __init__(self):
+        # create request header
+        self.headers = {'User-Agent': "rohith.mandavilli@gmail.com"}
+
+        # get all companies data
+        _ = requests.get(
+            "",
+            headers=self.headers
+        )
+
+        print("...retrieved model data...")
+
+async def main():
+    screener = ModelSelector()
+    while True:
+        user_input = input("Enter params? ")
+        if user_input.lower() == 'exit':
+            break
+
+        print('Model Analysis:')
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
